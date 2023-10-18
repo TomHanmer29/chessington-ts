2d915144938c10161f50440a55862aa00265e403
TomHan <Spacemarinedude29@gmail.com>
2023-10-18 17:12:58 +0100

    feat: added movement for the rook

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import gameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,18 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        const possibleMoves: Square[] = []
+        for (let rowNum = 0; rowNum < gameSettings.BOARD_SIZE; rowNum++) {
+            if(rowNum!=currentSquare.row){
+                possibleMoves.push(new Square(rowNum, currentSquare.col));
+            }
+        }
+        for (let colNum = 0; colNum < gameSettings.BOARD_SIZE; colNum++) {
+            if(colNum!=currentSquare.col){
+                possibleMoves.push(new Square(currentSquare.row, colNum));
+            }
+        }
+        return possibleMoves;
     }
 }
