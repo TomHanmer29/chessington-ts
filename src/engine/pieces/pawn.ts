37acb5610ad7b5b68222ba11b3fcf47de3cef5ad
TomHan <Spacemarinedude29@gmail.com>
2023-10-18 16:44:47 +0100

    Feat: Made pawns move 1-2 squares

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import player from "../player";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,19 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        const possibleMoves: Square[] = []
+        if (this.player===Player.WHITE) {
+            possibleMoves.push(new Square(currentSquare.row + 1, currentSquare.col));
+            if (currentSquare.row == 1) {
+                possibleMoves.push(new Square(currentSquare.row + 2, currentSquare.col));
+            }
+        } else {
+            possibleMoves.push(new Square(currentSquare.row - 1, currentSquare.col));
+            if (currentSquare.row == 6) {
+                possibleMoves.push(new Square(currentSquare.row - 2, currentSquare.col));
+            }
+        }
+        return possibleMoves;
     }
 }
