7e3be056cc5a68b1bac597fdc93a306d02e352e4
TomHan <Spacemarinedude29@gmail.com>
2023-10-19 11:15:43 +0100

    feat: add king movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import gameSettings from "../gameSettings";
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,32 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        const possibleMoves: Square[] = []
+        if(currentSquare.row+1<gameSettings.BOARD_SIZE){
+            possibleMoves.push(new Square(currentSquare.row+1,currentSquare.col))
+            if(currentSquare.col+1<gameSettings.BOARD_SIZE){
+                possibleMoves.push(new Square(currentSquare.row+1,currentSquare.col+1))
+            }
+            if(currentSquare.col-1>=0){
+                possibleMoves.push(new Square(currentSquare.row+1,currentSquare.col-1))
+            }
+        }
+        if(currentSquare.row-1>=0){
+            possibleMoves.push(new Square(currentSquare.row-1,currentSquare.col))
+            if(currentSquare.col+1<gameSettings.BOARD_SIZE){
+                possibleMoves.push(new Square(currentSquare.row-1,currentSquare.col+1))
+            }
+            if(currentSquare.col-1>=0){
+                possibleMoves.push(new Square(currentSquare.row-1,currentSquare.col-1))
+            }
+        }
+        if(currentSquare.col+1<gameSettings.BOARD_SIZE){
+            possibleMoves.push(new Square(currentSquare.row,currentSquare.col+1))
+        }
+        if(currentSquare.col-1>=0){
+            possibleMoves.push(new Square(currentSquare.row,currentSquare.col-1))
+        }
+        return possibleMoves;
     }
 }
