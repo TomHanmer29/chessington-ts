347ce9b746ebfe1ae85736bec1a4e06a2a84af8e
TomHan <Spacemarinedude29@gmail.com>
2023-10-19 10:59:57 +0100

    feat: add knight movement

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import gameSettings from "../gameSettings";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,40 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        const possibleMoves: Square[] = []
+        if(currentSquare.row+2<gameSettings.BOARD_SIZE){
+            if(currentSquare.col+1<gameSettings.BOARD_SIZE){
+                possibleMoves.push(new Square(currentSquare.row+2,currentSquare.col+1));
+            }
+            if(currentSquare.col-1>=0){
+                possibleMoves.push(new Square(currentSquare.row+2,currentSquare.col-1));
+            }
+        }
+        if(currentSquare.row-2>=0){
+            if(currentSquare.col+1<gameSettings.BOARD_SIZE){
+                possibleMoves.push(new Square(currentSquare.row-2,currentSquare.col+1));
+            }
+            if(currentSquare.col-1>=0){
+                possibleMoves.push(new Square(currentSquare.row-2,currentSquare.col-1));
+            }
+        }
+        if(currentSquare.col+2<gameSettings.BOARD_SIZE){
+            if(currentSquare.row+1<gameSettings.BOARD_SIZE){
+                possibleMoves.push(new Square(currentSquare.row+1,currentSquare.col+2));
+            }
+            if(currentSquare.row-1>=0){
+                possibleMoves.push(new Square(currentSquare.row-1,currentSquare.col+2));
+            }
+        }
+        if(currentSquare.col-2>=0){
+            if(currentSquare.row+1<gameSettings.BOARD_SIZE){
+                possibleMoves.push(new Square(currentSquare.row+1,currentSquare.col-2));
+            }
+            if(currentSquare.row-1>=0){
+                possibleMoves.push(new Square(currentSquare.row-1,currentSquare.col-2));
+            }
+        }
+        return possibleMoves;
     }
 }
