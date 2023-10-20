5d0f8afab1d289d3be6ffdf3cca8a7d711ee0299
TomHan <Spacemarinedude29@gmail.com>
2023-10-20 13:34:41 +0100

    refactor: tidied up the rook class

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -12,65 +12,43 @@ export default class Rook extends Piece {
     public getAvailableMoves(board: Board) {
         const currentSquare = board.findPiece(this)
         const possibleMoves: Square[] = []
-        let blockFlags = [false,false,false,false]
+        let blockFlags = [false, false, false, false]
+        let squareToCheck: Square
         for (let squareNum = 1; this.checkIsInBoard(squareNum); squareNum++) {
-            if(this.checkIsInBoard(currentSquare.row+squareNum)){
-                if(board.getPiece(Square.at(currentSquare.row+squareNum,currentSquare.col))===undefined && !blockFlags[0]) {
-                    possibleMoves.push(new Square(currentSquare.row + squareNum, currentSquare.col))
-                }
-                else if(board.getPiece(Square.at(currentSquare.row+squareNum,currentSquare.col))?.player != this.player && !blockFlags[0]
-                && !board.getPiece(Square.at(currentSquare.row+squareNum,currentSquare.col))?.isKing()){
-                    possibleMoves.push(new Square(currentSquare.row + squareNum, currentSquare.col))
-                    blockFlags[0] = true
-                }
-                else{
-                    blockFlags[0] = true
-                }
+            if (this.checkIsInBoard(currentSquare.row + squareNum) && !blockFlags[0]) {
+                squareToCheck = Square.at(currentSquare.row + squareNum, currentSquare.col)
+                blockFlags[0] = this.canSpaceBeMovedTo(board, squareToCheck, possibleMoves)
             }
-            if(this.checkIsInBoard(currentSquare.row-squareNum)){
-                if(board.getPiece(Square.at(currentSquare.row-squareNum,currentSquare.col))===undefined && !blockFlags[1]) {
-                    possibleMoves.push(new Square(currentSquare.row-squareNum, currentSquare.col))
-                }
-                else if(board.getPiece(Square.at(currentSquare.row-squareNum,currentSquare.col))?.player != this.player && !blockFlags[1]
-                    && !board.getPiece(Square.at(currentSquare.row-squareNum,currentSquare.col))?.isKing()){
-                    possibleMoves.push(new Square(currentSquare.row - squareNum, currentSquare.col))
-                    blockFlags[1] = true
-                }
-                else{
-                    blockFlags[1] = true
-                }
+            if (this.checkIsInBoard(currentSquare.row - squareNum) && !blockFlags[1]) {
+                squareToCheck = Square.at(currentSquare.row - squareNum, currentSquare.col)
+                blockFlags[1] = this.canSpaceBeMovedTo(board, squareToCheck, possibleMoves)
             }
-            if(this.checkIsInBoard(currentSquare.col+squareNum)){
-                if(board.getPiece(Square.at(currentSquare.row,currentSquare.col+squareNum))===undefined && !blockFlags[2]) {
-                    possibleMoves.push(new Square(currentSquare.row, currentSquare.col+squareNum))
-                }
-                else if(board.getPiece(Square.at(currentSquare.row,currentSquare.col+squareNum))?.player != this.player && !blockFlags[2]
-                    && !board.getPiece(Square.at(currentSquare.row,currentSquare.col+squareNum))?.isKing()){
-                    possibleMoves.push(new Square(currentSquare.row, currentSquare.col+squareNum))
-                    blockFlags[2] = true
-                }
-                else{
-                    blockFlags[2] = true
-                }
+            if (this.checkIsInBoard(currentSquare.col + squareNum) && !blockFlags[2]) {
+                squareToCheck = Square.at(currentSquare.row, currentSquare.col + squareNum)
+                blockFlags[2] = this.canSpaceBeMovedTo(board, squareToCheck, possibleMoves)
             }
-            if(this.checkIsInBoard(currentSquare.col-squareNum)){
-                if(board.getPiece(Square.at(currentSquare.row,currentSquare.col-squareNum))===undefined && !blockFlags[3]) {
-                    possibleMoves.push(new Square(currentSquare.row, currentSquare.col-squareNum))
-                }
-                else if(board.getPiece(Square.at(currentSquare.row,currentSquare.col-squareNum))?.player != this.player && !blockFlags[3]
-                    && !board.getPiece(Square.at(currentSquare.row,currentSquare.col-squareNum))?.isKing()){
-                    possibleMoves.push(new Square(currentSquare.row, currentSquare.col-squareNum))
-                    blockFlags[3] = true
-                }
-                else{
-                    blockFlags[3] = true
-                }
+            if (this.checkIsInBoard(currentSquare.col - squareNum) && !blockFlags[3]) {
+                squareToCheck = Square.at(currentSquare.row, currentSquare.col - squareNum)
+                blockFlags[3] = this.canSpaceBeMovedTo(board, squareToCheck, possibleMoves)
             }
         }
         return possibleMoves;
     }
 
-    public isKing(){
+    public isKing() {
         return false
+    }
+
+    public canSpaceBeMovedTo(board: Board, squareToCheck: Square, possibleMoves: Square[]) {
+        let blockFlag = false
+        if (board.getPiece(squareToCheck) === undefined) {
+            possibleMoves.push(squareToCheck)
+        } else if (board.getPiece(squareToCheck)?.player != this.player && !board.getPiece(squareToCheck)?.isKing()) {
+            possibleMoves.push(squareToCheck)
+            blockFlag = true
+        } else {
+            blockFlag = true
+        }
+        return blockFlag
     }
 }
