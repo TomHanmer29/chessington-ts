fbaed9baa3f0b91680f82ea3dbad278c2ee58a45
TomHan <Spacemarinedude29@gmail.com>
2023-10-20 11:42:27 +0100

    feat: prevented pawns moving off the board

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -13,12 +13,16 @@ export default class Pawn extends Piece {
         const currentSquare = board.findPiece(this)
         const possibleMoves: Square[] = []
         if (this.player===Player.WHITE) {
-            possibleMoves.push(new Square(currentSquare.row + 1, currentSquare.col));
+            if(this.checkIsInBoard(currentSquare.row+1)){
+                possibleMoves.push(new Square(currentSquare.row + 1, currentSquare.col));
+            }
             if (currentSquare.row == 1) {
                 possibleMoves.push(new Square(currentSquare.row + 2, currentSquare.col));
             }
         } else {
-            possibleMoves.push(new Square(currentSquare.row - 1, currentSquare.col));
+            if(this.checkIsInBoard(currentSquare.row-1)){
+                possibleMoves.push(new Square(currentSquare.row - 1, currentSquare.col));
+            }
             if (currentSquare.row == 6) {
                 possibleMoves.push(new Square(currentSquare.row - 2, currentSquare.col));
             }
